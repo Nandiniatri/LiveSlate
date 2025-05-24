c8a36d285bb600b56911f2ba824032740c67cb4d
Nandiniatri <atrinandini8@gmail.com>
2025-05-24 17:38:58 +0530

    chatbox and canvas import and there css add in my project

--- src/pages/LiveSlate.jsx
+++ src/pages/LiveSlate.jsx
@@ -1,9 +1,24 @@
+import Canvas from "./canvas/Canvas";
+import ChatBox from "./chatBox/ChatBox";
 import Header from "./header/Header";
+import VideoCall from "./videoCall/VideoCall";
+
 
 const LiveSlate = () => {
     return (
         <div className="liveSlate-main-container">
             <Header />
+            <div className="liveSlate-div2">
+                <div className="video-call-section">
+                    <VideoCall />
+                </div>
+                <div className="canvas-section">
+                    <Canvas />
+                </div>
+                <div className="chat-section">
+                    <ChatBox />
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/pages/videoCall/VideoCall.jsx
@@ -0,0 +1,2 @@
+const VideoCall = () => <div><h3>Video Call</h3></div>;
+export default VideoCall; 
