467dbea86ebae2b0c011ba2a0953047f9290bead
Nandiniatri <atrinandini8@gmail.com>
2025-06-04 14:48:33 +0530

    add room id in props

--- src/pages/LiveSlate.jsx
+++ src/pages/LiveSlate.jsx
@@ -26,4 +26,4 @@ const LiveSlate = ({ roomID }) => {
 };
 
 export default LiveSlate;
- 
+  
--- src/pages/videoCall/VideoCall.jsx
+++ src/pages/videoCall/VideoCall.jsx
@@ -536,14 +536,16 @@ import { v4 as uuid } from "uuid";
 
 const socket = io("https://videocallbackend-rjrw.onrender.com"); // your backend
 
-const ROOM_ID = "classroom-101"; // static room (or make dynamic)
 
-const VideoCall = () => {
+const VideoCall = ({roomID}) => {
   const localVideoRef = useRef(null);
   const localStreamRef = useRef(null);
   const peersRef = useRef({});
   const [remoteStreams, setRemoteStreams] = useState({});
   const userId = useRef(uuid());
+  const ROOM_ID = `${roomID}`; // static room (or make dynamic);
+  console.log(ROOM_ID);;
+  
 
   useEffect(() => {
     const init = async () => {
