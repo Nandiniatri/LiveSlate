59ee915513ab266d1af05bb3b2437f63c098c131
Nandiniatri <atrinandini8@gmail.com>
2025-06-04 16:37:55 +0530

    write wimdow.location.origin

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,34 +1,3 @@
-// import './HomePage.css';
-// import { useNavigate } from 'react-router-dom';
-
-// const Home = () => {
-//   const navigate = useNavigate();
-
-//   const handleJoinRoomId = () => {
-//     const roomID = crypto.randomUUID();
-//     console.log(roomID);
-
-//     navigate(`/room/${roomID}`);
-//   }
-
-//   return (
-//     <div className="home-container">
-//       <div className="glass-card">
-//         <h1 className="title">LiveSlate</h1>
-//         <p className="subtitle">Collaborate live with video, chat & canvas</p>
-//         <div className="button-group">
-//           <button onClick={handleJoinRoomId} className="btn join-btn">Join Room</button>
-//           <button onClick={() => navigate('/createRoom')} className="btn create-btn">Create Room</button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-
-
 import './HomePage.css';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
