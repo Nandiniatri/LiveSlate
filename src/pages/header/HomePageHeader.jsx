231fb87caf4f93b3fac6c2abfd687268ee0ca689
Nandiniatri <atrinandini8@gmail.com>
2025-06-21 12:39:14 +0530

    add puctute in homepageHeader

--- src/pages/header/HomePageHeader.jsx
+++ src/pages/header/HomePageHeader.jsx
@@ -1,46 +1,3 @@
-// import { useState } from "react";
-// import Button from "../../components/Button";
-// import SignUpPage from "../signUpPage/SignUp";
-// import { Link } from "react-router-dom";
-// import ModalB from "../../components/ModalB";
-// import { useUsername } from "../../context/UsernamePrompt";
-
-// const HomePageHeader = () => {
-//     const { user, handleLogout } = useUsername();
-//     console.log('User object:', user);
-
-
-//     return (
-//         <div className="header-wrapper">
-//             <div className="header-container">
-//                 <h2 className="header-title">LiveSlate</h2>
-//                 <div className="toolbar">
-//                     {user ? (
-//                         <>
-//                             <img
-//                                 src={user.user_metadata.avatar_url}
-//                                 style={{ width: 80, borderRadius: '50%', marginBottom: 10 }}
-//                             />
-//                             <p>{user.user_metadata.full_name}</p>
-//                             <button className="logout-button" onClick={handleLogout}>
-//                                 Logout
-//                             </button>
-//                         </>
-//                     ) : (
-//                         <Button className="home-header-btn"><Link to={'/signUp'}>Sign up</Link></Button>
-//                     )}
-//                 </div>
-//             </div>
-
-//         </div>
-//     );
-// };
-
-// export default HomePageHeader;
-
-
-
-
 import { useState } from "react";
 import Button from "../../components/Button";
 import { Link } from "react-router-dom";
@@ -60,7 +17,7 @@ const HomePageHeader = () => {
                     {user ? (
                         <div className="avatar-wrapper">
                             <img
-                                src={user.user_metadata.avatar_url}
+                                src={user.user_metadata.picture}
                                 style={{ width: 50, height: 50, borderRadius: "50%", cursor: "pointer" }}
                                 onClick={() => setShowProfileModal(!showProfileModal)}
                             />
