7e510b2158f0889e078240a0dd4a998c6eef8288
Nandiniatri <atrinandini8@gmail.com>
2025-06-21 11:53:02 +0530

    add css in modal

--- src/pages/header/HomePageHeader.jsx
+++ src/pages/header/HomePageHeader.jsx
@@ -1,14 +1,54 @@
+// import { useState } from "react";
+// import Button from "../../components/Button";
+// import SignUpPage from "../signUpPage/SignUp";
+// import { Link } from "react-router-dom";
+// import ModalB from "../../components/ModalB";
+// import { useUsername } from "../../context/UsernamePrompt";
+
+// const HomePageHeader = () => {
+//     const { user, handleLogout } = useUsername();
+//     console.log('User object:', user);
+
+
+//     return (
+//         <div className="header-wrapper">
+//             <div className="header-container">
+//                 <h2 className="header-title">LiveSlate</h2>
+//                 <div className="toolbar">
+//                     {user ? (
+//                         <>
+//                             <img
+//                                 src={user.user_metadata.avatar_url}
+//                                 style={{ width: 80, borderRadius: '50%', marginBottom: 10 }}
+//                             />
+//                             <p>{user.user_metadata.full_name}</p>
+//                             <button className="logout-button" onClick={handleLogout}>
+//                                 Logout
+//                             </button>
+//                         </>
+//                     ) : (
+//                         <Button className="home-header-btn"><Link to={'/signUp'}>Sign up</Link></Button>
+//                     )}
+//                 </div>
+//             </div>
+
+//         </div>
+//     );
+// };
+
+// export default HomePageHeader;
+
+
+
+
 import { useState } from "react";
 import Button from "../../components/Button";
-import SignUpPage from "../signUpPage/SignUp";
 import { Link } from "react-router-dom";
-import ModalB from "../../components/ModalB";
 import { useUsername } from "../../context/UsernamePrompt";
 
 const HomePageHeader = () => {
     const { user, handleLogout } = useUsername();
-    console.log('User object:', user);
-
+    const [showProfileModal, setShowProfileModal] = useState(false);
 
     return (
         <div className="header-wrapper">
@@ -19,22 +59,40 @@ const HomePageHeader = () => {
                         <>
                             <img
                                 src={user.user_metadata.avatar_url}
-                                style={{ width: 80, borderRadius: '50%', marginBottom: 10 }}
+                                style={{ width: 80, borderRadius: "50%", cursor: "pointer" }}
+                                onClick={() => setShowProfileModal(true)}
                             />
-                            {/* <p>{user.user_metadata.full_name}</p> */}
-                            <button className="logout-button" onClick={handleLogout}>
-                                Logout
-                            </button>
+                            <p>{user.user_metadata.full_name}</p>
                         </>
                     ) : (
-                        <Button className="home-header-btn"><Link to={'/signUp'}>Sign up</Link></Button>
+                        <Button className="home-header-btn">
+                            <Link to={"/signUp"}>Sign up</Link>
+                        </Button>
                     )}
                 </div>
             </div>
 
+            {/* Modal */}
+            {showProfileModal && (
+                <div className="profile-modal">
+                    <div className="modal-content">
+                        <img
+                            src={user.user_metadata.avatar_url}
+                            style={{ width: 80, borderRadius: "50%", marginBottom: 10 }}
+                            alt="avatar"
+                        />
+                        <p>{user.user_metadata.full_name}</p>
+                        <button className="logout-button" onClick={handleLogout}>
+                            Logout
+                        </button>
+                        <button className="close-button" onClick={() => setShowProfileModal(false)}>
+                            Close
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
 
 export default HomePageHeader;
-
