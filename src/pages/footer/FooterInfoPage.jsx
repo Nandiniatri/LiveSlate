d64e96171f93438f6a06192607e3ec210ea06eaf
Nandiniatri <atrinandini8@gmail.com>
2025-06-23 15:48:18 +0530

    add string in json id

--- src/pages/footer/FooterInfoPage.jsx
+++ src/pages/footer/FooterInfoPage.jsx
@@ -1,14 +1,23 @@
+import { useParams } from "react-router-dom";
 import { useUsername } from "../../context/UsernamePrompt";
 
 const FooterInfoPage = () => {
     const { footerNav } = useUsername();
+    const { id } = useParams();
+
+    const selectedItem = footerNav.find((item) => item.id === id);
+    console.log(selectedItem);
+    
+
+    if (!selectedItem) {
+        return <h2>No Item Found</h2>
+    }
+
 
     return (
         <div>
             <h1>Hello</h1>
-            {footerNav.map((footItm) => (
-                <h2>{footItm.name}</h2>
-            ))}
+
         </div>
     )
 }
